16dd37ceb79da43d8aa2412eac3da82cc046fbb0
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-06-29 12:39:03 -0400

    app-flask

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+
+@app.route('/')
+def main():
+    return jsonify({ "message": "api flask"}), 200
 
 if __name__ == '__main__':
     app.run()
